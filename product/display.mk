cdd793033f0bd403a3c4a60d7cc2c7239662bbd4
iGOR121 <chernokalsky89@gmail.com>
2021-03-23 19:52:29 +0300

    comment unbuildable strings

--- product/display.mk
+++ product/display.mk
@@ -17,4 +17,4 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+# $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
