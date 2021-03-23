0eae932f022977cee995b25e4a2c692d2d88bbee
iGOR121 <chernokalsky89@gmail.com>
2021-03-23 19:45:26 +0300

    unsuccessful try to fix the camera

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -1,19 +1,21 @@
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    camera.device@3.2-impl \
-    Snap
+    camera.msm8916 \
+    libmm-qcamera \
+    SnapdragonCamera
 
 # Build libstlport for vendor blobs
-#PRODUCT_PACKAGES += \
-#    libstlport
+PRODUCT_PACKAGES += \
+    libstlport
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/vendor/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/native/data/etc/android.hardware.camera.front.xml:system/vendor/etc/permissions/android.hardware.camera.front.xml
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.cpp.duplication=false \
-    persist.camera.hal.debug.mask=0
+    persist.camera.hal.debug.mask=0 \
+    media.stagefright.legacyencoder=true \
+    media.stagefright.less-secure=true
